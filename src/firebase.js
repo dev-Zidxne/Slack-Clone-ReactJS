dec46ed24cccfd4ce25182c360ecd9de61fe91ef
Zidane Innis <zidaneinnis@gmail.com>
2022-08-20 00:49:53 -0500

    Set up Rirebase and React Router

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDwkDpzol1cAg3Z7JZ0Qiy06ZkSXfuTMqQ",
+  authDomain: "slack-clone-reactjs-4de85.firebaseapp.com",
+  projectId: "slack-clone-reactjs-4de85",
+  storageBucket: "slack-clone-reactjs-4de85.appspot.com",
+  messagingSenderId: "945647303105",
+  appId: "1:945647303105:web:0440900a68194b6be7d740",
+};
