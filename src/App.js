6651e50d7cb08909f1f45e3f54c02b37bcde5d19
Zidane Innis <zidaneinnis@gmail.com>
2022-08-26 00:35:14 -0500

    Building the Sidebar Component

--- src/App.js
+++ src/App.js
@@ -7,21 +7,29 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
-
+import styled from "styled-components";
+import Sidebar from "./components/Sidebar";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route path="/" exact>
-            <Route path="/" element={<Header />} />
-          </Route>
-        </Routes>
+        <AppBody>
+          <Routes>
+            <Route path="/" exact>
+              <Route path="/" element={<Header />} />
+            </Route>
+            <Route path="/" exact>
+              <Route path="/" element={<Sidebar />} />
+            </Route>
+          </Routes>
+        </AppBody>
       </Router>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div``;
--- src/components/HeaderSearch.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function HeaderSearch() {
-  return <div>HeaderSearch</div>;
-}
-
-export default HeaderSearch;
