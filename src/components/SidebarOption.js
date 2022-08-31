08533c4389458bf90373dd12cb183156fb3c89a4
Zidane Innis <zidaneinnis@gmail.com>
2022-08-30 22:15:07 -0500

    Channels not rendering issue to be resolved

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,8 +1,22 @@
 import React from "react";
 import styled from "styled-components";
-function SidebarOption({ Icon, title }) {
+
+import { db } from "../firebase";
+
+function SidebarOption({ Icon, title, addChannelOption, id }) {
+  const addChannel = () => {
+    const channelName = prompt("Please enter the channel name");
+    if (channelName) {
+      db.collection("rooms").add({
+        name: channelName,
+      });
+    }
+  };
+  const selectChannel = () => {};
   return (
-    <SidebarOptionContainer>
+    <SidebarOptionContainer
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
       {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
       {Icon ? (
         <h3>{title}</h3>
@@ -27,6 +41,16 @@ const SidebarOptionContainer = styled.div`
     opacity: 0.9;
     background-color: #340e36;
   }
+  > h3 {
+    font-weight: 500;
+  }
+
+  > h3 > span {
+    padding: 15px;
+  }
 `;
 
-const SidebarOptionChannel = styled.div``;
+const SidebarOptionChannel = styled.h3`
+  padding: 10px 0;
+  font-weight: 300;
+`;
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app"; //v9
+import "firebase/compat/auth"; //v9
+import { GoogleAuthProvider } from "firebase/auth";
+import "firebase/compat/firestore"; //v9
 const firebaseConfig = {
   apiKey: "AIzaSyDwkDpzol1cAg3Z7JZ0Qiy06ZkSXfuTMqQ",
   authDomain: "slack-clone-reactjs-4de85.firebaseapp.com",
@@ -6,3 +10,11 @@ const firebaseConfig = {
   messagingSenderId: "945647303105",
   appId: "1:945647303105:web:0440900a68194b6be7d740",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider, firebaseApp };
