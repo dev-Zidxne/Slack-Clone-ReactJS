74e56cd4a330f3344c7c30a8f9a6627812496dd6
Zidane Innis <zidaneinnis@gmail.com>
2022-09-01 17:36:25 -0500

    Implemented Google sign in functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,20 @@
+import { Button } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
+import { auth, provider } from "../firebase";
 
 function Login() {
+  const signIn = (e) => {
+    e.preventDefault();
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
   return (
     <LoginContainer>
       <LoginInnerContainer>
         <img src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg" />
+        <h1>Sign in to The Bad Group</h1>
+        <p>bad.slack.com</p>
+        <Button onClick={signIn}>Sign in with Google</Button>
       </LoginInnerContainer>
     </LoginContainer>
   );
@@ -25,10 +34,16 @@ const LoginInnerContainer = styled.div`
   text-align: center;
   background-color: white;
   border-radius: 10px;
-
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   > img {
     object-fit: contain;
     height: 100px;
     margin-bottom: 40px;
+  }
+  > button {
+    margin-top: 50px;
+    text-transform: inherit !important;
+    background-color: #0a8d48 !important;
+    color: white;
   }
 `;
